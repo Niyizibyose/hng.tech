ea509ad0a2f7b830d19bf0b9de2600a85411cab1
Niyizibyose <47078766+Niyizibyose@users.noreply.github.com>
2019-04-05 14:50:35 +0200

    Add files via upload

---
+++ Confident.php
@@ -0,0 +1,38 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <h6>Skills</h6>
+                        <img src="https://res.cloudinary.com/confident/image/upload/c_scale,h_100,w_100/v1554467157/PROF.jpg" alt="Jean Confident Irenee NIYIZIBYOSE">
+                        <h6> Jean Confident Irenee NIYIZIBYOSE</h6>
+                        <p> UX Designer </p>
+                        <ul class="list">
+                           <li><a href="#" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="#" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="#" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p> I am a seasoned Data scientist building my problem solving skills. I started as a UX Designer and I am building my problem solving skills to bridge the gap between an idea and a product. I believe that I will be part of the most innovative engineering teams in Africa</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>Jupyter | Python</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
